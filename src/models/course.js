0cc04eff7128a951c8657524c72b34c8e50a05b0
nicolasjhampton <nicolasjhampton@gmail.com>
2016-08-19 02:13:11 -0700

    Todo: final error formatting + password validation

--- src/models/course.js
+++ src/models/course.js
@@ -30,6 +30,33 @@ CourseSchema.pre('save', function(next) {
   return next();
 });
 
+CourseSchema.path('reviews').validate(function(reviews, callback) {
+  var valid = true;
+  var course = this;
+  var counter = 0;
+  for(var i = 0; i < reviews.length; i++) {
+    Review.findById(reviews[i])
+          .populate('user')
+          .select('user')
+          .exec(function(err, reviewObj) {
+            counter++;
+
+            if(!reviewObj || !course.user._id) return callback(true);
+            
+            var reviewId = reviewObj.user._id;
+
+            if(reviewId.toString() !== course.user._id.toString() && valid == true) {
+              valid = false;
+            }
+
+            if(counter == (reviews.length - 1)) {
+              console.log('result:', valid);
+              return callback(valid);
+            }
+          });
+  }
+}, "A user cannot post a review to their own course.");
+
 CourseSchema.path('steps').validate(function(steps) {
   return !(!steps || steps.length === 0);
 }, "At least one step is required");
